10ff4a263fd9db468eaa74d893c5425fd1ab160c
Hector Daniel Perez <hdpmontreal@hotmail.com>
2012-02-16 10:55:54 -0300

    version 1

---
+++ include/Persona.h
@@ -0,0 +1,14 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+
+class Persona
+{
+    public:
+        Persona() {}
+        virtual ~Persona() {}
+    protected:
+    private:
+};
+
+#endif // PERSONA_H
